280df5e20e412a0ccc695d3cbf87d16a8f9d67d9
McManOfTheLand <forrestchew@gmail.com>
2021-11-21 10:14:30 -0800

    Updated contract to generic token so anyone can add name, symbol

---
+++ contracts/ERC20Token.sol
@@ -0,0 +1,53 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.7.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@chainlink/contracts/src/v0.7/VRFConsumerBase.sol";
+
+contract ERC20Token is ERC20, VRFConsumerBase, Ownable {
+    address vrfCoordinator;
+    bytes32 keyhash;
+    address linkToken;
+    uint256 fee;
+    uint256 randomResult;
+    // address[] public guiMinters;  
+    mapping(address => bool) public isMinter;
+    event RandomNumberFulfillment(bytes32 requestId);
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        address _vrfCoordinator,
+        address _linkToken,
+        bytes32 _keyhash,
+        uint256 _fee
+    ) 
+    VRFConsumerBase(
+        vrfCoordinator,
+        linkToken
+    ) 
+    ERC20(_name, _symbol)
+    {
+        vrfCoordinator = _vrfCoordinator;
+        linkToken = _linkToken;
+        keyhash = _keyhash;
+        fee = _fee;
+    }
+
+    function getRandomNumber() private onlyMinter() returns(bytes32 requestId) {
+        require(LINK.balanceOf(address(this)) >= fee, "Please fund contract with Link");
+        return requestRandomness(keyhash, fee);
+    }
+
+    function fulfillRandomness(bytes32 requestId, uint256 randomness) internal override {
+        require(randomness > 0, "Randomness not returned");
+        randomResult = (randomness % 15) + 1;
+        _mint(msg.sender, randomResult);
+        emit RandomNumberFulfillment(requestId);
+    }
+
+    modifier onlyMinter() {
+        require(isMinter[msg.sender] == true, "Has to be minter");
+        _;
+    }
+}
