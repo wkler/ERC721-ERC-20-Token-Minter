834d271e79a96552351d2b53f52c27930100bc71
McManOfTheLand <forrestchew@gmail.com>
2021-11-26 16:47:52 -0800

    changed contract constructor name and symbol inputs to placeholders. Also updated comments and formatting

--- scripts/deploy_contracts.py
+++ scripts/deploy_contracts.py
@@ -1,12 +1,17 @@
+#This file handles contract deployment.
+#See contract constructors for input field types
+#See config file for more info on arguments passed to contract constructors
+#To run this file, type: brownie run PATH/deploy_contract.py 
+#Make sure to have your virtual environment activated
 from brownie import ERC20Token, NftFactory, config, network
 from scripts.utils import(
     get_account, LOCAL_BLOCKCHAIN_ENVIROMENTS
 )
-
-#Verifiys contracts on etherscan automatically.  
+#Verifiys contracts on etherscan automatically when given as constructor argument
 publish_source = (
     True if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIROMENTS else False
 )
+#Deploys NFT factory contract with specified arguments
 def deploy_nft():
     account = get_account()
     nft_factory = NftFactory.deploy(
@@ -16,11 +21,12 @@ def deploy_nft():
         publish_source=publish_source
     )
 
+#Deploys ERC20Token contract with specified arguments
 def deploy_token():
     account = get_account() 
     token_contract = ERC20Token.deploy(
-        "GUIIIIIIIIII",
-        "GUI",
+        "NameHere",
+        "SymbolHere",
         config["networks"][network.show_active()]["vrf_coordinator_address"],
         config["networks"][network.show_active()]["link_token_address"],
         config["networks"][network.show_active()]["key_hash"],
