3db24a20aa7e5809ff6d9a9506e2981c47fb8cf9
McManOfTheLand <forrestchew@gmail.com>
2022-02-11 12:15:27 -0800

    Reformatted

--- scripts/start_gui.py
+++ scripts/start_gui.py
@@ -1,5 +1,7 @@
-# Run this script with <brownie run scripts/start_gui.py --network some network> assuming you are in the projects root directory.
-# This script gets it's own file incase more start up functionality needs to be added
+# Run this script with <brownie run scripts/start_gui.py --network some network>
+# assuming you are in the projects root directory.
 from Gui.gui_main import Main
+
+
 def main():
     Main()
