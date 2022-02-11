0a9745d29c93535dfbce1b1caefd22fc0b582201
McManOfTheLand <forrestchew@gmail.com>
2022-02-11 12:14:04 -0800

    refactored code to be more concise

--- scripts/mint_tokens.py
+++ scripts/mint_tokens.py
@@ -1,12 +1,12 @@
-# This scripts is meant to ONLY be run when the "MINT" button is clicked within the gui
-# This script calls the "mintTokens" function on PATH/TO/ERC20Token.sol contract anytime the "MINT" button is clicked within the gui
-# The acmount of tokens minted to contract owner will always be between 1 - 15
 from brownie import ERC20Token
 from scripts.utils import get_account
 
+# Mints from 1 - 15 ERC-20 tokens to the NFT minter.
 def mint_erc20_tokens():
     account = get_account()
+    # Gets latest instance of the ERC-20 token contract
     token_contract = ERC20Token[-1]
     print(f"  Minting tokens from {token_contract}")
-    tx = token_contract.mintTokens({"from": account})
+    # Calls the function that mints tokens to the function invoker
+    token_contract.mintTokens({"from": account})
     print(f"  Chainlink VRF called\nVerifying random number...\n")
