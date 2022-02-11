7ce47a0e684d9f8d27b3ec02d2dd85e21ce8a577
McManOfTheLand <forrestchew@gmail.com>
2022-02-11 12:15:48 -0800

    refactored to be more concise

--- scripts/upload_image_to_ipfs.py
+++ scripts/upload_image_to_ipfs.py
@@ -1,25 +1,25 @@
-import sys
-sys.path.append("metadata_info")
 import json
 import requests
-from metadata_template import nft_metadata
-from brownie import NftFactory, ERC20Token
-from scripts.utils import get_account, network, config
+from pathlib import Path
 from metadata_template import nft_metadata
 
+# Uploads image to IPFS and and updates NFT metadata to reflect the image URI
 def ipfs_upload():
     filepath = "./stylized.png"
-    with open(filepath, "rb") as styled_image_path:
+    # Opens path to image and reads it
+    with Path(filepath).open("rb") as styled_image_path:
         image_binary = styled_image_path.read()
         ipfs_url = "http://127.0.0.1:5001"
         endpoint = "/api/v0/add"
+        # Posts image to IPFS. This image will become the NFT image
         response = requests.post(ipfs_url + endpoint, files={"file": image_binary})
         ipfs_hash = response.json()["Hash"]
         filename = filepath.split("/")[-1:][0]
+        # Initializes variable with the full IPFS link to image
         image_uri = f"https://ipfs.io/ipfs/{ipfs_hash}?filename={filename}"
-    with open("metadata_info/metadata.json", "w") as updated_metadata_image:
+
+    # Open metadata.json file and dumps the metadata dictionary into it
+    with Path("./metadata_info/metadata.json").open("w") as updated_metadata_image:
         nft_metadata["image"] = image_uri
         json.dump(nft_metadata, updated_metadata_image, indent=2)
         return image_uri
-
-
