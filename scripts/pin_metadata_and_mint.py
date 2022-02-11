af01d7f61fa06c231bf0327f739fc9362507459e
McManOfTheLand <forrestchew@gmail.com>
2022-02-11 10:48:19 -0800

    Formatted and commented

--- scripts/pin_metadata_and_mint.py
+++ scripts/pin_metadata_and_mint.py
@@ -1,3 +1,7 @@
+# Pins NFT metadata, as well as the NFT image to Pinata.
+# Once pinned, the mint function within the NFT factory contract
+# is called, and the Pinata link to the NFT metadata is
+# passed as parameter in order to mint.
 import os
 import requests
 from pathlib import Path
@@ -6,38 +10,48 @@ from scripts.utils import get_account
 
 PINATA_BASE_URL = "https://api.pinata.cloud/"
 endpoint = "pinning/pinFileToIPFS"
-filepath = "./metadata_info/metadata.json"
-filename = filepath.split("/")[-1:][0]
+nft_metadata_filepath = "./metadata_info/metadata.json"
+nft_metadata_filename = nft_metadata_filepath.split("/")[-1:]
+nft_img_to_pin = "./stylized.png"
+# Authorization information for Pinata
 headers = {
     "pinata_api_key": os.getenv("PINATA_API_KEY"),
     "pinata_secret_api_key": os.getenv("PINATA_API_SECRET"),
 }
 
+# Pins both the NFT metadata and the NFT image to Pinata
 def upload_to_pinata():
-    with Path(filepath).open("rb") as fp:
-        image_binary = fp.read()
+    # Opens and reads the NFT image
+    with Path(nft_img_to_pin).open("rb") as nft_image:
+        image = nft_image.read()
+        # Pins the NFT image to Pinata
         response = requests.post(
             PINATA_BASE_URL + endpoint,
-            files={"file": (filename, image_binary)},
+            files={"file": ("nft_image", image)},
             headers=headers,
         )
-        print(response.json()["IpfsHash"])
+    # Opens and reads NFT metatdata
+    with Path(nft_metadata_filepath).open("rb") as fp:
+        nft_meta_data = fp.read()
+        # Pins the NFT metadata to Pinata
+        response = requests.post(
+            PINATA_BASE_URL + endpoint,
+            files={"file": (nft_metadata_filename, nft_meta_data)},
+            headers=headers,
+        )
         meatadata_ipfs_hash = response.json()["IpfsHash"]
+        # Inserts the IPFS hash provided in our POST response to the Pinata link
         pinned_metadata_url = f"https://gateway.pinata.cloud/ipfs/{meatadata_ipfs_hash}"
-    mint_nft_metadata(pinned_metadata_url)
-    print(f"  Begining to mint {pinned_metadata_url}")
+        print(f"  Minting {pinned_metadata_url}")
+        # Invokes the NFT minting function within smart contract and passes it the pinned metadata URL
+        mint_nft_metadata(pinned_metadata_url)
 
-def mint_nft_metadata(ipfs_hash):
-    print(ipfs_hash)
+
+# Mints pinned metadata URL
+def mint_nft_metadata(nft_metadata):
     account = get_account()
+    # Gets the most recent nft factory contract instance
     nft_factory_contract = NftFactory[-1]
-    nft_factory_contract.createNFT(
-        ipfs_hash,
-        {"from": account}
-    )
-    print(f"  This is your {nft_factory_contract.tokenCounter()}th collectable!")
-    
-# def main():
-#     upload_to_pinata()
-
-    
+    # Mints pinned metadata URL
+    nft_factory_contract.createNFT(nft_metadata, {"from": account})
+    print(f"You now have {nft_factory_contract.tokenCounter()} collectable")
