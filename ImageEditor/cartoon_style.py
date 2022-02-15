2b9ba1fd0997e20c95fd538c6d986029f39bec96
McManOfTheLand <forrestchew@gmail.com>
2022-02-15 14:29:39 -0800

    Refactored code slightly

--- ImageEditor/cartoon_style.py
+++ ImageEditor/cartoon_style.py
@@ -1,20 +1,21 @@
 import cv2 as cv
+from pathlib import Path
 
+# Applies cartoon style to image
 def cartoonify_image():
-    image_path = "TextFiles/image_path.txt"
-    file = open(image_path, "r")
-    image_string = file.read()
-    image = cv.imread(image_string, cv.IMREAD_COLOR)
+    # Reads image taken from the image path
+    with Path("TextFiles/image_path.txt").open("r") as image_path:
+        read_image = image_path.read()
+        image = cv.imread(read_image, cv.IMREAD_COLOR)
+        # Converts image to gray in order to apply adaptive thresholding
+        convert_img_to_gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+        # Blurs image
+        convert_img_to_gray = cv.medianBlur(convert_img_to_gray, 5)
+        adaptive_threshold = cv.adaptiveThreshold(
+            convert_img_to_gray, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 7, 7
+        )
+        dst = cv.edgePreservingFilter(image, flags=2, sigma_s=64, sigma_r=0.5)
+        complete_style = cv.bitwise_and(dst, dst, mask=adaptive_threshold)
 
-    edges1 = cv.bitwise_not(cv.Canny(image, 100, 200))
-    convert_img_to_gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-    convert_img_to_gray = cv.medianBlur(convert_img_to_gray, 5)
-
-    adaptive_threshold = cv.adaptiveThreshold(
-        convert_img_to_gray, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 7, 7
-    )
-    dst = cv.edgePreservingFilter(image, flags=2, sigma_s=64, sigma_r=0.5)
-    complete_style = cv.bitwise_and(dst, dst, mask=adaptive_threshold)
-
-    new_style_filename = "stylized.png"
-    cv.imwrite(new_style_filename, complete_style)
+        new_style_filename = "stylized.png"
+        cv.imwrite(new_style_filename, complete_style)
